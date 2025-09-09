c50bbce2e5d4617aae8d9c3d490504901e215776
padmaja0793 <padmaja7210@gmail.com>
2025-09-09 10:56:45 +0100

    JSCODES Updates

---
+++ First.js
@@ -0,0 +1,21 @@
+//console.log("Welcome to uss");
+//console.log("Padmaja");
+//console.log("I love js");
+
+//Fullname = "Padmaja Pujari"
+//console.log(Fullname);
+
+//age = 22;
+//console.log("age");
+
+//price = 98.43;
+//console.log(price);
+
+//x = null;
+y = undefined;
+
+console.log(y);
+
+
+
+
